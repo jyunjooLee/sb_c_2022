bde12c28a6c3e268ee57a402db2a9b6534e1a039
jyunjooLee <ecaruss@naver.com>
2022-03-11 16:28:57 +0900

    count 조절 Action구현

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -11,33 +11,18 @@ public class UsrHomeController {
 		count = -1;
 	}
 	
-	@RequestMapping("/usr/home/main") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
+	@RequestMapping("/usr/home/getCount") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
 	@ResponseBody						/* ShowMain 실행결과를 response 한다. */
-	public String ShowMain() {
-		return "안녕하세요.";
-	}
-	@RequestMapping("/usr/home/main2") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
-	@ResponseBody						/* ShowMain 실행결과를 response 한다. */
-	public String ShowMain2() {
-		return "반갑습니다.";
-	}
-	@RequestMapping("/usr/home/main3") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
-	@ResponseBody						/* ShowMain 실행결과를 response 한다. */
-	public String ShowMain3() {
-		return "또 만나요.";
-	}
-	@RequestMapping("/usr/home/main4") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
-	@ResponseBody						/* ShowMain 실행결과를 response 한다. */
-	public int ShowMain4() {
+	public int getCount() {
 		count++;
 		return count; 
 	}
 	
-	@RequestMapping("/usr/home/main5") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
+	@RequestMapping("/usr/home/doSetCount") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
 	@ResponseBody						/* ShowMain 실행결과를 response 한다. */
-	public String ShowMain5() {
-		count= -1;
-		return "Count의 값이 0으로 초기화"; 
+	public String doSetCount(int count) {
+		this.count = count;
+		return "Count의 값이 "+this.count+"으로 초기화"; 
 	}
 	
 }
