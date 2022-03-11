94d0aa6ad6ddcc53ed5fc91d0f1efa8bb59d719a
jyunjooLee <ecaruss@naver.com>
2022-03-11 14:26:59 +0900

    /usr/home/main4 추가

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller /* 스프링에게 이것이 Controller라는걸 알려준다.   */
 public class UsrHomeController {
+	private int count;
+	public UsrHomeController() {
+		count = -1;
+	}
+	
 	@RequestMapping("/usr/home/main") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
 	@ResponseBody						/* ShowMain 실행결과를 response 한다. */
 	public String ShowMain() {
@@ -20,5 +25,11 @@ public class UsrHomeController {
 	@ResponseBody						/* ShowMain 실행결과를 response 한다. */
 	public String ShowMain3() {
 		return "또 만나요.";
+	}
+	@RequestMapping("/usr/home/main4") /* 이렇게 url이 들어오면 아래 함수(ShowMain)를 실행한다. */
+	@ResponseBody						/* ShowMain 실행결과를 response 한다. */
+	public int ShowMain4() {
+		count++;
+		return count; 
 	}
 }
